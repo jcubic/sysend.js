868369ecf93636e038fce1119a6a79a1762578a3
Jakub Jankiewicz <jcubic@onet.pl>
2022-08-21 16:45:40 +0200

    update version

--- sysend.js
+++ sysend.js
@@ -1,5 +1,5 @@
 /**@license
- *  sysend.js - send messages between browser windows/tabs version 1.12.1
+ *  sysend.js - send messages between browser windows/tabs version 1.12.2
  *
  *  Copyright (C) 2014-2022 Jakub T. Jankiewicz <https://jcubic.pl/me>
  *  Released under the MIT license
