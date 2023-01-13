69b1d08d806412148d56290e43323792ad596c4a
Jakub Jankiewicz <jcubic@onet.pl>
2023-01-13 12:06:45 +0100

    update type definitions

--- sysend.d.ts
+++ sysend.d.ts
@@ -5,24 +5,25 @@
  *  Released under the MIT license
  *
  */
-type callback = (message: any, event: string) => void;
+type callback = (message: unknown, event: string) => void;
 
 interface Sysend {
     id: string;
-    broadcast(event: string, data?: any): void;
-    emit(event: string, data?: any): void;
+    broadcast(event: string, data?: unknown): void;
+    emit(event: string, data?: unknown): void;
     on(event: string, callback: callback): void;
     off(event: string, callback?: callback): void;
     proxy(...args: string[]): void;
-    serializer(to: (data: any) => string, from: (data: string) => any): void;
+    serializer(to: (data: unknown) => string, from: (data: string) => unknown): void;
     track(event: 'open', callback: (data: {id: string, count: number, primary: boolean}) => void): void;
     track(event: 'close', callback: (data: {id: string, count: number, primary: boolean, self: boolean}) => void): void;
     track(event: 'primary', callback: () => void): void;
-    track(event: 'message', callback: (payload: {data: any, origin: string}) => void): void;
+    track(event: 'message', callback: (payload: {data: unknown, origin: string}) => void): void;
     track(event: 'secondary', callback: () => void): void;
-    untrack(event: 'open' | 'close' | 'primary' | 'secondary' | 'message', fn?: (input?: any) => void): void;
+    track(event: 'update', callback: (payload: Array<{ id: string, primary: boolean }>) => void): void;
+    untrack(event: 'open' | 'close' | 'primary' | 'secondary' | 'message' | 'update', fn?: (input?: unknown) => void): void;
     list(): Promise<Array<{ id: string, primary: boolean }>>;
-    post(target: string, data?: any): void;
+    post(target: string, data?: unknown): void;
     channel(...domains: string[]): void;
     isPrimary(): boolean;
     rpc<T extends Array<unknown>, U>(object: Record<string, (...args: T) => U>): Promise<Record<string, (id: string, ...args: T) => Promise<U>>>
