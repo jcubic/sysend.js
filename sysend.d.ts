369499b43ac6f67abdd55b3ad5e1d369326fb82f
Jakub Jankiewicz <jcubic@onet.pl>
2023-12-12 13:21:42 +0100

    update version

--- sysend.d.ts
+++ sysend.d.ts
@@ -1,5 +1,5 @@
 /**@license
- *  sysend.js - send messages between browser windows/tabs version 1.17.0
+ *  sysend.js - send messages between browser windows/tabs version 1.17.1
  *
  *  Copyright (C) 2014-2023 Jakub T. Jankiewicz <https://jcubic.pl/me>
  *  Released under the MIT license
